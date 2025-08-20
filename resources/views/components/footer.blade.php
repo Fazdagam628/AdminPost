596860919241faa1a0ba26012164fe11c0b6b738
Fazdagam628 <fauzanmasud628@gmail.com>
2025-08-20 09:16:46 +0700

    commenting footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,8 +4,8 @@
             <p>© 2025 PPLG - Made By SMKN 11 Semarang</p>
         </div>
         <div class="footer-right">
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms of Service</a>
+            <!-- <a href="#">Privacy Policy</a>
+            <a href="#">Terms of Service</a> -->
         </div>
     </div>
 </footer>
